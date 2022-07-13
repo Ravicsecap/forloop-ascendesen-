9f538cf84cc1643be3e583406db0b7bc860252db
Ravi sahani <108450291+Ravicsecap@users.noreply.github.com>
2022-07-13 14:03:20 +0530

    Add files via upload

---
+++ AsceDsce.java
@@ -0,0 +1,20 @@
+
+public class AsceDsce {
+
+	      public static void main(String[] args) {
+	     System.out.println(" Asending order:-");
+	 
+for(int i=1;i<=10;i++)
+	 {
+		 System.out.println(i);
+		 
+	 }
+	      System.out.println(" Desending order:-");
+for(int k=10;k>=1;k--)
+	 {
+		 System.out.println(k);
+	 }
+
+	}
+
+}
